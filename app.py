5d5b0ac2874526ecc8ad4a26dac6948b6219cf1f
wuyungang <jameswuxiaomo@gmail.com>
2023-05-31 16:20:42 +0900

    update

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ import numpy as np
 from PIL import Image
 
 
-st.balloons()
+st.sidebar.title('目录')
+st.sidebar.tabs(['测试页面1', '测试页面2'])
 title_pic = Image.open('pics/title.png')
 st.image(title_pic)
 st.write('2023.5.31 Build1 :sunglasses:')
