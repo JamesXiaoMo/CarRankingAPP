66f22021830894ac4e566da3b426b1d8bdc670a7
wuyungang <jameswuxiaomo@gmail.com>
2023-05-31 14:19:26 +0900

    update

--- app.py
+++ app.py
@@ -7,3 +7,5 @@ from PIL import Image
 title_pic = Image.open('pics/title.png')
 st.image(title_pic)
 st.text('2023.5.31 Build1')
+price = st.slider('价格区间(万)', 0, 1000, 160)
+st.write('当前价格区间为' + price)
