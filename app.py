adccc2f13dbc984947203d08d64a977e23cdb14d
wuyungang <jameswuxiaomo@gmail.com>
2023-05-31 13:53:44 +0900

    update

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+from PIL import Image
 
 
 st.title('Car Ranking App')
-title_pic = open('pics/title.png')
+title_pic = Image.open('pics/title.png')
 st.image(title_pic)
